513bf4f529228210f9649a837777cab6a840d88b
nthach <nich.thach@hotmail.com>
2022-09-19 09:40:36 -0700

    Define classes in model.py and Registered Songs and Rest Framework in Project Settings Installed Apps Section + execute migrations

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,14 @@
+from platform import release
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.IntegerField()
+    release = models.DateField()
+    genre = models.CharField(max_length=255)
+
+
